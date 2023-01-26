679adcffaac8eca8e9fd58f568066472864262d9
JoshSJKim <seonjoon.josh.kim@gmail.com>
2023-01-26 13:25:41 -0500

    Minor syntax edit

--- Counting-Cards.js
+++ Counting-Cards.js
@@ -8,14 +8,14 @@
     case 4:
     case 5:
     case 6:
-       count++;
+      count++;
       break;
     case 10:
     case 'J':
     case 'Q':
     case 'K':
     case 'A':
-       count--;
+      count--;
       break;
   }
   if (count > 0) {
