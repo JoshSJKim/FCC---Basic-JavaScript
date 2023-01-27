edd748fa23ddd07827795fd654d062df8a40f491
JoshSJKim <seonjoon.josh.kim@gmail.com>
2023-01-27 14:41:47 -0500

    Add solution to record collection js file

--- Record_collection.js
+++ Record_collection.js
@@ -21,7 +21,22 @@ const recordCollection = {
   
   // Only change code below this line
   function updateRecords(records, id, prop, value) {
+    if (prop !== "tracks" && value !== "") {
+        records[id][prop]=value;
+    } else if (prop === "tracks" && records[id].hasOwnProperty("tracks") === false) {
+        records[id][prop]= [value];
+    } else if (prop === "tracks" && value !== "") {
+        records[id][prop].push(value);
+    } else if (value === "") {
+        delete records[id][prop];
+    }
+    }
     return records;
-  }
   
   updateRecords(recordCollection, 5439, 'artist', 'ABBA');
+
+/* Your function must always return the entire record collection object.
+If prop isn't tracks and value isn't an empty string, update or set that album's prop to value.
+If prop is tracks but the album doesn't have a tracks property, create an empty array and add value to it.
+If prop is tracks and value isn't an empty string, add value to the end of the album's existing tracks array.
+If value is an empty string, delete the given prop property from the album.*/
