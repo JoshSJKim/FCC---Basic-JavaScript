78c8f694e41c99f79570117a4d15b773876d09d9
JoshSJKim <seonjoon.josh.kim@gmail.com>
2023-01-27 18:24:34 -0500

    Add solution to for_loop_iteration js file

--- for_loop_iteration.js
+++ for_loop_iteration.js
@@ -4,7 +4,7 @@ const myArr = [2, 3, 4, 5, 6];
 // Only change code below this line
 let total = 0;
 for (let i=0; i<myArr.length; i++) {
-
+total += myArr[i];
 }
  
 /* Declare and initialize a variable total to 0. 
